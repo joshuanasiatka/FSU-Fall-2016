0cb0ab7209b3e1d7c9564d26a99f42f87809e281
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-09-08 12:52:16 -0400

    Swap3x function
    `C++` function that swaps three pre-defined variables

--- oop/wk01/swap3x/swap3x.cpp
+++ oop/wk01/swap3x/swap3x.cpp
@@ -4,3 +4,30 @@
   * @author  Joshua Nasiatka
   * @date    Sept 08 2016
   */
+
+#include<iostream>
+
+using namespace std;
+
+template<class T>
+void swap(T&x, T&y, T&z);
+
+int main() {
+  int i=3, j=4, k=5;
+
+  cout << i << " " << j << " " << k << endl;
+  swap(i,j,k);
+  cout << i << " " << j << " " << k << endl;
+
+  return 0;
+}
+
+template<class T>
+void swap(T&x, T&y, T&z) {
+  T temp;
+
+  temp = z;
+  z = y;
+  y = x;
+  x = temp;
+}
