62de96e69318c7c7479f314ce0ffe82183433f1d
Joshua Nasiatka <joshua@bitcraftlabs.net>
2016-12-12 22:19:13 -0500

    Completed Project 2

--- tpcs-sci-comp/projects/mandelbrot/Mandelbrot.jl
+++ tpcs-sci-comp/projects/mandelbrot/Mandelbrot.jl
@@ -1,17 +1,19 @@
 module Mandelbrot
 
-export MandelbrotView, newton, iterate, is_in_mbset, leaving_number, plot
+export MandelbrotView, newton, iterate, is_in_mbset, leaving_number, plot, plot_zoom
 
 using Images, Colors, ImageView
 
 type MandelbrotView
   min::Number
   max::Number
-  N::Int64
+  #N::Int64
+  nx::Int64
+  ny::Int64
 
-  MandelbrotView()=new(Complex(0),Complex(0),0); #throw(ArgumentError("Invalid Mandelbrot: Need at least a min and max"))
-  MandelbrotView(min, max)=new(Complex(min),Complex(max),255);
-  MandelbrotView(min, max, N)=new(Complex(min),Complex(max),N);
+  MandelbrotView()=new(-2-im,1+im,600,400); #throw(ArgumentError("Invalid Mandelbrot: Need at least a min and max"))
+  MandelbrotView(min, max)=new(Complex(min),Complex(max),600,400);
+  MandelbrotView(min, max, nx, ny)=new(Complex(min),Complex(max),nx,ny);
 end
 
 function newton(f,df,x0)
@@ -30,7 +32,7 @@ function iterate(z::Number,iter::Int64=255)
   local arr=Complex[]
   local lnum = 255
   for n = 1:leaving
-    c = z
+    # c = z
     if abs(z) > 2
       lnum = n
       return arr, lnum
@@ -44,15 +46,14 @@ function iterate(z::Number,iter::Int64=255)
 end
 
 function is_in_mbset(c::Complex)
-  local mb = iterate(c)
-  return mb[2] == 255
+  return iterate(c)[2] == 255
 end
 
 function leaving_number(c::Complex,iter::Int64=10,ipoint::Complex=0+0im)
-  local out = iterate(c,iter);
-  return out[2]
+  return iterate(c,iter)[2];
 end
 
+#=
 function plot(m::MandelbrotView,scale::Number=1)
   local height = 400*scale
   local width = 600*scale
@@ -60,6 +61,41 @@ function plot(m::MandelbrotView,scale::Number=1)
   local y=map(x->leaving_number(x),z)
   y=map(x->UInt8(x),y)
   return ImageCmap(y,colormap("RdBu",256))
+end
+
+
+function complex_newton(zmin::Complex, zmax::Complex,nx::Int=50,ny::Int=50)
+    local dx = ((real(zmax)-real(zmin))/(nx-1))
+    local dy = ((imag(zmin)-imag(zmax))/(ny-1))
+    local Z=[x+y*im for y=imag(zmax):dy:imag(zmin), x=real(zmin):dx:real(zmax)]
+    local roots = map(a->newton(z->z^3-1,z->3z^2,a),Z)
+    local angles = round(map(angle,roots)*180/pi)
+    angles[find(isnan,angles)]=0.0
+    local datas=div(convert(Array{Int,2},angles)+240,120)
+    local cmap = colormap("Blues", 3)
+    return ImageCmap(transpose(datas),cmap)
+end
+=#
+
+function plot(xy::MandelbrotView)
+    local dx = (real(xy.max)-real(xy.min))/(xy.nx-1)
+    local dy = (imag(xy.min)-imag(xy.max))/(xy.ny-1)
+    local Z=[x+im*y for y = imag(xy.max):dy:imag(xy.min),x=real(xy.min):dx:real(xy.max)]
+    local img_map = (map(x->(leaving_number(x,255)),Z))
+    local cmap = colormap("RdBu", 256)
+    return ImageCmap(transpose(img_map),cmap)
+end
+
+function plot_zoom(xy::MandelbrotView,shiftUD::Number=0,shiftLR::Number=0,zoom::Number=1.0)
+    -1<=shiftUD&&shiftUD<=1?true:throw(ArgumentError("Invalid Up/Down Shift: Must be between -1 and 1"))
+    -2<=shiftLR&&shiftLR<=1?true:throw(ArgumentError("Invalid Left/Right Shift: Must be between -2 and 1"))
+    local dx = (real(xy.max)-real(xy.min))/(xy.nx-1)
+    local dy = (imag(xy.min)-imag(xy.max))/(xy.ny-1)
+    local Z=[(x+(shiftLR*zoom))+im*(y+(shiftUD*zoom)) for y = imag(xy.max):dy:imag(xy.min),x=real(xy.min):dx:real(xy.max)]
+    Z*=abs(1/zoom)
+    local img_map = (map(x->(leaving_number(x,255)),Z))
+    local cmap = colormap("RdBu", 256)
+    return ImageCmap(transpose(img_map),cmap)
 end
 
 end
--- tpcs-sci-comp/projects/mandelbrot/mandelbrot_test.jl
+++ tpcs-sci-comp/projects/mandelbrot/mandelbrot_test.jl
@@ -3,4 +3,30 @@ if !("." in LOAD_PATH)
     push!(LOAD_PATH,".")
 end
 
-using Mandelbrot
+using Mandelbrot, Base.Test
+
+## test the creation of a MandelbrotViews
+
+m1 = MandelbrotView()
+m2 = MandelbrotView(-2-im,1+im)
+
+n1 = -im
+n2 = -1.01im
+n3 = 0.4+0.0im
+n4 = -0.5+0.5im
+n5 = 2im
+
+@testset "Mandelbrot Tests" begin
+
+    @testset "Mandelbrot Constructor" begin
+        @test isa(m1,MandelbrotView)
+        @test isa(m2,MandelbrotView)
+    end
+    @testset "In Mandelbrot Set" begin
+        @test is_in_mbset(n1)
+        @test !is_in_mbset(n2)
+        @test !is_in_mbset(n3)
+        @test is_in_mbset(n4)
+        @test !is_in_mbset(n5)
+    end
+end
